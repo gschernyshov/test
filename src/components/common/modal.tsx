014b35b0627746f52de1d7fcf8c8a33dbf12d8ce
gschernyshov <grchafv@yandex.ru>
2025-10-08 15:55:45 +0300

    Обновлены модальные окна и Header, фикс скролла и крестика на мобильных

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -40,12 +40,12 @@ const CustomModal = ({ isOpen, onClose, size = "md", children }: IProps) => {
       size={isMobile ? "full" : size}
       classNames={{
         base: isMobile
-          ? "fixed top-0 left h-screen w-screen overflow-y-auto rounded-none py-4 px-4" 
-          : "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-sm w-full max-h-[90vh] overflow-y-auto rounded-lg bg-white dark:bg-default-200 py-5 px-6",     
+          ? "fixed inset-0 w-screen h-screen rounded-none overflow-y-auto pt-20 px-4"
+          : "fixed inset-0 max-w-sm max-h-[90vh] overflow-y-auto py-10 px-5 m-auto rounded-lg",
         closeButton: isMobile
-          ? "fixed top-4 right-4 z-50 p-2 text-2xl text-gray-500 hover:text-black"
-          : "absolute top-2 right-2 p-2 text-lg text-gray-500 hover:text-black",
-        body: "overflow-y-auto",
+          ? "absolute top-4 right-4 z-50 p-2 text-2xl text-gray-500 hover:text-black"
+          : "absolute top-2 right-2 z-50 p-2 text-lg text-gray-500 hover:text-black",
+        body: "overflow-y-auto max-h-full",
       }}
     >
       <ModalContent>
