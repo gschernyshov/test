85683e730474913bbb2ad5d4737de1ba8273b428
gschernyshov <grchafv@yandex.ru>
2025-10-08 15:46:43 +0300

    Обновлены модальные окна и Navbar для мобильной версии

--- src/components/common/modal.tsx
+++ src/components/common/modal.tsx
@@ -40,11 +40,11 @@ const CustomModal = ({ isOpen, onClose, size = "md", children }: IProps) => {
       size={isMobile ? "full" : size}
       classNames={{
         base: isMobile
-          ? "relative h-screen w-screen rounded-none overflow-y-auto py-4 px-4" 
-          : "relative max-w-sm max-h-[90vh] overflow-y-auto py-10 px-5",        
+          ? "fixed top-0 left h-screen w-screen overflow-y-auto rounded-none py-4 px-4" 
+          : "fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 max-w-sm w-full max-h-[90vh] overflow-y-auto rounded-lg bg-white dark:bg-default-200 py-5 px-6",     
         closeButton: isMobile
-          ? "absolute top-4 right-4 z-50 m-4 p-2 text-2xl text-gray-500 hover:text-black"
-          : "absolute top-2 right-2 z-50 p-2 text-lg text-gray-500 hover:text-black",
+          ? "fixed top-4 right-4 z-50 p-2 text-2xl text-gray-500 hover:text-black"
+          : "absolute top-2 right-2 p-2 text-lg text-gray-500 hover:text-black",
         body: "overflow-y-auto",
       }}
     >
